37f295cd5e26306748cae592dd677cd7ac0f86cf
sigma <89007924+Shakarovich@users.noreply.github.com>
2023-06-13 18:15:28 +0500

    signin error(500)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from "./auth.service";
+import {AuthDto} from "./dto";
+import {Tokens} from "./types";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+    @Post('/local/signup')
+    signupLocal(@Body() dto: AuthDto): Promise<Tokens>{
+      return  this.authService.signUpLocal(dto)
+    }
+    @Post('/local/signin')
+    signinLocal(@Body() dto: AuthDto): Promise<Tokens>{
+      return  this.authService.signInLocal(dto)
+    }
+    @Post('/logout')
+    logout(){
+        return this.authService.logout()
+    }
+    @Post('/refresh')
+    refreshTokens(){
+     return this.authService.refreshTokens()
+    }
+}
